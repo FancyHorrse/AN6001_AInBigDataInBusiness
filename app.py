919d41e45a8255dbd187c22c925e553edec5f61e
XXXY <114380769+FancyHorrse@users.noreply.github.com>
2025-11-21 01:57:50 +0000

    25.11.21 Change 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return render_template("main.html")
 
 
 if __name__ == "__main__":
