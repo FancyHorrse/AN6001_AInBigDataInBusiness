3dc60a9a8addee132ec5d08ce397654fb43cb70c
XXXY <114380769+FancyHorrse@users.noreply.github.com>
2025-11-21 03:09:36 +0000

    25.11.21 Change 3

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import joblib
+
+# import scikit-learn as sklearn
 
 app = Flask(__name__)
 
@@ -13,6 +16,22 @@ def main():
     q = request.form.get("q")
     print(q)
     return render_template("main.html")
+
+
+@app.route("/dbs", methods=["GET", "POST"])
+def dbs():
+    return render_template("dbs.html")
+
+
+@app.route("/dbs_prediction", methods=["GET", "POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    print(q)
+
+    model = joblib.load("./dbs.pkl")
+    r = model.predict([[q]])
+
+    return render_template("dbs_prediction.html", r=r[0])
 
 
 if __name__ == "__main__":
